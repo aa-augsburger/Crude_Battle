133effcd9a483f01f12a6aeb6876e3514e2b12bf
Antoine <aa.augsburger@gmail.com>
2026-05-21 15:14:07 +0200

    refactor maps with enum

--- src/main/scala/Game/Maps.scala
+++ src/main/scala/Game/Maps.scala
@@ -1,68 +1,67 @@
 package Game
 
+
+import Game.Terrain.Air
 import ch.hevs.gdx2d.lib.GdxGraphics
 import com.badlogic.gdx.graphics.Color
-sealed trait TerrainType
 
-object TerrainType {
-  case object Air extends TerrainType
-  case object Terre extends TerrainType
-  case object Roche extends TerrainType
+
+object Terrain extends Enumeration {
+  case class TerrainVal(solidity: Int, color: Color) extends super.Val {
+    def drawBlock(g: GdxGraphics, x: Int, y: Int): Unit = {
+      g.setColor(color)
+      g.drawFilledRectangle(
+        x.toFloat,
+        y.toFloat,
+        blockSize.toFloat,
+        blockSize.toFloat,
+        0f
+      )
+    }
+  }
+
+  import scala.language.implicitConversions
+  implicit def valueToPlanetVal(x: Value): TerrainVal = x.asInstanceOf[TerrainVal]
+
+  val blockSize = 2
+  val Dirt = TerrainVal(1, Color.GREEN)
+  val Rock = TerrainVal(3, Color.GRAY)
+  val Air = TerrainVal(0, Color.BLUE)
 }
-//enum TerrainType(solidite: Int, color: Color):
-//case Air extends Type(0)
-//case Terre extends Color(1)
-//case Pierre extends Color(2)
-class Maps{
-  import TerrainType._
+
+
+class Maps {
+
   val largeur: Int = 2048
   val hauteur: Int = 1080
   val tailleBloc: Int = 4
-  private val grille: Array[Array[TerrainType]] =
+  val grille: Array[Array[Terrain.TerrainVal]] =
     Array.fill(largeur, hauteur)(Air)
+
   def initMaps(): Unit = {
-    for (x <- 0 until largeur;
-         y <- 0 until hauteur) {
+    for (x <- 0 until largeur; y <- 0 until hauteur) {
       grille(x)(y) =
-        if (y < 10) {
-          Roche
+        if (y < 50) {
+          Terrain.Rock
         }
-        else if (y < 500) {
-          Terre
+        else if (y >= 50 && y < 500) {
+          Terrain.Dirt
         }
         else {
-          Air
+          Terrain.Air
         }
     }
     println(s"Map créée : ${largeur}x${hauteur}")
   }
+
   def refreshMaps(g: GdxGraphics): Unit = {
     for (x <- 0 until largeur by tailleBloc;
          y <- 0 until hauteur by tailleBloc) {
-      grille(x)(y) match {
-        case Roche =>
-          g.setColor(Color.GRAY)
-          g.drawFilledRectangle(
-            x.toFloat,
-            y.toFloat,
-            tailleBloc.toFloat,
-            tailleBloc.toFloat,
-            0f
-          )
-        case Terre =>
-          g.setColor(Color.GREEN)
-          g.drawFilledRectangle(
-            x.toFloat,
-            y.toFloat,
-            tailleBloc.toFloat,
-            tailleBloc.toFloat,
-            0f
-          )
-        case Air =>
-        // videee
-      }
+      grille(x)(y).drawBlock(g, x, y)
     }
   }
+
+
   //fonction destruction de terrain
   //defndestruction(x: Int, y: Int, radius: Int): Unit = {
   //  for (i <- -radius to radius;
