7f393ac7ca4ed750c9ed1b80cc86ec837f4837ea
Jaures <jauresbeinjamin@gmail.com>
2026-05-21 13:04:29 +0200

    Cretion tableau

--- src/main/scala/Game/Main.scala
+++ src/main/scala/Game/Main.scala
@@ -3,5 +3,5 @@ package Game
 object Main extends App{
   val game = new Game()
   game.run()
-
+val maps = new Maps()
 }
--- src/main/scala/Game/Maps.scala
+++ src/main/scala/Game/Maps.scala
@@ -1,6 +1,11 @@
 package Game
 
-class Maps {
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.color
 
+class Maps {
+//Mise en place de la map
+  //creation tableau  2D de 10x10
+  val map = Array.ofDim[Int](1,10)
 
 }
